acb974f1bbcfef9b2f3d08422a37e4781dd28ec0
Carlee Hansen <chansen@unc.edu>
2024-03-05 21:21:40 -0700

    made new exceptions

---
+++ shared/src/main/java/exception/ErrorHandle.java
@@ -0,0 +1,29 @@
+package exception;
+
+import com.google.gson.Gson;
+import exception.ResponseException;
+import spark.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ErrorHandle {
+
+    public static String handleException(Exception e, Response res) {
+        Map<String, String> errorResponse = new HashMap<>();
+
+        // Check if the exception is an instance of ResponseException to handle custom exceptions
+        if (e instanceof ResponseException) {
+            ResponseException re = (ResponseException) e;
+            res.status(re.StatusCode()); // Set the specific status code from the exception
+            errorResponse.put("message", re.getMessage());
+        } else {
+            // For general exceptions, set a 500 status code
+            res.status(500);
+            errorResponse.put("message", "Error: Internal Server Error");
+        }
+
+        // Convert the error response map to JSON
+        return new Gson().toJson(errorResponse);
+    }
+}
