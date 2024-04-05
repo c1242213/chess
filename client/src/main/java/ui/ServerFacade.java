a172d64069934052f24c26ffc454a23f475e0c4d
Carlee Hansen <chansen@unc.edu>
2024-04-05 16:02:38 -0600

    finished tests

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.IOException;
+
 import java.net.*;
 
 public class ServerFacade {
