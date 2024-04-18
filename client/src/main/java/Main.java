fd5a5ad3ade68950a1f5dc602490962b1a28d05e
Carlee Hansen <chansen@unc.edu>
2024-04-18 00:00:34 -0600

    finished?

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -5,7 +5,7 @@ import ui.PreLoginUI;
 public class Main {
     public static void main(String[] args) throws ResponseException {
 
-        var serverUrl = "http://localhost:8080";
+        var serverUrl = "http://localhost: 8080";
         PreLoginUI preLogin = new PreLoginUI(serverUrl);
         preLogin.preLoginMenu();
     }
